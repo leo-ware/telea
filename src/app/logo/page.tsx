330e8af7e5a07c4cedf624485bba6e54a75dbd89
Leo Ware <leoware@uni.minerva.edu>
2024-12-05 12:38:37 -0500

    new favicon

---
+++ src/app/logo/page.tsx
@@ -0,0 +1,9 @@
+import Logo from "@/components/Logo"
+
+export default function LogoPage() {
+    return (
+        <div className="m-4 w-[180px] h-[180px] flex justify-center items-center bg-[#219ebc]">
+            <Logo width={176} />
+        </div>
+    )
+}
